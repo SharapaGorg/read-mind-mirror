591163f7b912c351f9267823284c33b40b97974a
shara <sharapov.savely@yandex.ru>
2025-10-29 10:31:24 +0300

    chore: updated package-lock.json

--- bro.config.js
+++ bro.config.js
@@ -6,18 +6,17 @@ module.exports = {
   apiPath: 'stubs/api',
   webpackConfig: {
     output: {
-      publicPath: `/static/${pkg.name}/${process.env.VERSION || pkg.version}/`
-    }
+      publicPath: `/static/${pkg.name}/${process.env.VERSION || pkg.version}/`,
+    },
   },
   /* use https://admin.bro-js.ru/ to create config, navigations and features */
   navigations: {
     'read-mind-mirror.main': '/read-mind-mirror',
   },
   features: {
-    'ui-sample-project': {
+    'read-mind-mirror': {
       // add your features here in the format [featureName]: { value: string }
     },
   },
-  config: {
-  }
+  config: {},
 }
